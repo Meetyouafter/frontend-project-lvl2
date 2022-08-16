846e59ae41cb47d239b9f23fe76402ce9df18690
Meetyouafter <anton_levus@mail.ru>
2022-08-16 14:37:16 +0300

    preparatio project

---
+++ gendiff.js
@@ -0,0 +1,11 @@
+import programm from 'command'
+
+program
+  .option('--first')
+  .option('-s, --separator <char>');
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
