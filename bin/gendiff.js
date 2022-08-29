e10d2fa884314fab32462853929b9a29ddf07177
Meetyouafter <anton_levus@mail.ru>
2022-08-29 22:05:30 +0300

    add lodach add file1.json add file2.json edit gendiff.js - create logic for comparison files without commander

---
+++ bin/gendiff.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+ 
+import { program } from 'commander';
+import path from 'path';
+import { readFileSync } from  'node:fs';
+
+const getFilesDifference = (file1, file2) => {
+  
+const filepath1 = path.resolve('file1.json');
+const filepath2 = path.resolve('file2.json');
+
+const dataOfFile1 = JSON.parse(readFileSync(filepath1));
+const dataOfFile2 = JSON.parse(readFileSync(filepath2));
+
+const copyOfFile1ForMerge = Object.assign({}, dataOfFile1);
+const mergedFilesData = Object.assign(copyOfFile1ForMerge, dataOfFile2);
+
+let result = '';
+const sortedKeys = Object.keys(mergedFilesData).sort();
+  for (const key of sortedKeys) {
+    if (dataOfFile1[key] === dataOfFile2[key]) {
+      result += `\n    ${key}: ${dataOfFile1[key]}`;
+    }  if (dataOfFile1[key] === undefined) {
+      result += `\n  + ${key}: ${dataOfFile2[key]}`;
+    }  if (dataOfFile2[key] === undefined) {
+      result += `\n  - ${key}: ${dataOfFile1[key]}`;
+    }  if ((dataOfFile1[key] !== dataOfFile2[key]) && dataOfFile1[key] !== undefined && dataOfFile2[key] !== undefined) {
+      result += `\n  - ${key}: ${dataOfFile1[key]}\n  + ${key}: ${dataOfFile2[key]}`;
+    }
+  }
+console.log(`{${result}\n}`);
+return `{${result}\n}`;
+};
+
+program
+  .version('0.0.1')
+  .argument('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .action(getFilesDifference)
+
+program.parse();
