d45f7480cae720d98acade4ff532b9bc936ff0a0
Meetyouafter <anton_levus@mail.ru>
2022-09-03 15:51:30 +0300

    edit gendiff.js rename getDifferense.js to getDifference.js add some rules to node.js.yml

--- __fixtures__/getDifference.js
+++ __fixtures__/getDifference.js
@@ -2,26 +2,22 @@ import path, { dirname } from 'path';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 
-// eslint-disable-next-line no-unused-vars
 const getFilesDifference = (filepath1, filepath2) => {
-  const fileName1 = 'file1.json';
-  const fileName2 = 'file2.json';
-
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
   const getFilePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-  const dataOfFile1 = JSON.parse(readFileSync(getFilePath(fileName1)));
-  const dataOfFile2 = JSON.parse(readFileSync(getFilePath(fileName2)));
+  const dataOfFile1 = JSON.parse(readFileSync(getFilePath(filepath1)));
+  const dataOfFile2 = JSON.parse(readFileSync(getFilePath(filepath2)));
 
   const copyOfFile1ForMerge = { ...dataOfFile1 };
   const mergedFilesData = Object.assign(copyOfFile1ForMerge, dataOfFile2);
 
   let result = '';
   const sortedKeys = Object.keys(mergedFilesData).sort();
-  // eslint-disable-next-line no-restricted-syntax
-  for (const key of sortedKeys) {
+
+  sortedKeys.forEach((key) => {
     if (dataOfFile1[key] === dataOfFile2[key]) {
       result += `\n    ${key}: ${dataOfFile1[key]}`;
     } if (dataOfFile1[key] === undefined) {
@@ -32,7 +28,8 @@ const getFilesDifference = (filepath1, filepath2) => {
     && dataOfFile2[key] !== undefined) {
       result += `\n  - ${key}: ${dataOfFile1[key]}\n  + ${key}: ${dataOfFile2[key]}`;
     }
-  }
+  });
+
   console.log(`{${result}\n}`);
   return `{${result}\n}`;
 };
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import getFilesDifference from '../__fixtures__/getDifferense.js';
+import getFilesDifference from '../__fixtures__/getDifference.js';
 
 program
   .version('0.0.1')
-  .argument('<filepath1> <filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
   .action(getFilesDifference);
